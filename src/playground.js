3c6ec1e36233c8329db8e653e4d0c217011a9f10
V.Mironov <kuomolew@gmail.com>
2022-08-24 18:17:29 +0300

    objects

--- src/playground.js
+++ src/playground.js
@@ -1,5 +1,24 @@
-const fruits = ["Apple", "Banana", "Orange"];
-const vegetables = ["Cucumber", "Radish"];
+/** *********** Arrays ************** */
+// const fruits = ["Apple", "Banana", "Orange"];
+// const vegetables = ["Cucumber", "Radish"];
 
-console.log([...fruits, ...vegetables]);
-console.log([fruits, vegetables]);
+// console.log([...fruits, ...vegetables]);
+// console.log([fruits, vegetables]);
+
+/** *********** Objects ************** */
+
+// const developer = {
+//   salary: 100000,
+//   experience: 4.5,
+//   techStack: ["Vue", "HTML", "CSS"],
+//   lookingForWork: true,
+//   doubleSalary() {
+//     this.salary *= 2;
+//     this.lookingForWork = false;
+//   },
+// };
+
+// console.log(developer);
+// developer.doubleSalary();
+
+// console.log(developer);
