6dc3abef26f4949383106a7b76d4ce24386b6534
V.Mironov <kuomolew@gmail.com>
2022-08-24 17:34:27 +0300

    eslinter fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "prettier"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
   },
   overrides: [
     {
