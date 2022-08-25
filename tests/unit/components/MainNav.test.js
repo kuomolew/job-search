8e1e67abdfe295a59bba2ba00ce030cc0f80ce64
V.Mironov <kuomolew@gmail.com>
2022-08-25 17:53:37 +0300

    delete setData code

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -3,11 +3,8 @@ import { mount } from "@vue/test-utils";
 import MainNav from "@/components/MainNav";
 
 describe("MainNav", () => {
-  it("displays company name", async () => {
+  it("displays company name", () => {
     const wrapper = mount(MainNav);
-    await wrapper.setData({
-      company: "Super Corp",
-    });
     expect(wrapper.text()).toMatch("Careers");
   });
 });
