48280e46d691eaef58a91d97c6e4ed2403b9cb9f
V.Mironov <kuomolew@gmail.com>
2022-08-25 19:40:29 +0300

    test menu items linked to li tag

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,20 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Careers");
   });
+
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll("li");
+
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    // console.log(navigationMenuTexts);
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Corp",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
