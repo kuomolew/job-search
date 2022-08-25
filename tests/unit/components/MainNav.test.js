f08d470104918dd15994e14c8c2b62b0870caaec
V.Mironov <kuomolew@gmail.com>
2022-08-25 17:50:41 +0300

    setData method

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -3,8 +3,11 @@ import { mount } from "@vue/test-utils";
 import MainNav from "@/components/MainNav";
 
 describe("MainNav", () => {
-  it("displays company name", () => {
+  it("displays company name", async () => {
     const wrapper = mount(MainNav);
+    await wrapper.setData({
+      company: "Super Corp",
+    });
     expect(wrapper.text()).toMatch("Careers");
   });
 });
