5455ae40b3cfa847be72559b0e0644fca16d6691
V.Mironov <kuomolew@gmail.com>
2022-08-25 15:24:15 +0300

    playground code refactor

--- src/playground.js
+++ src/playground.js
@@ -23,8 +23,6 @@
 
 // console.log(developer);
 
-export const evenOrOdd = (number) => {
-  return number % 2 === 0 ? "Even" : "Odd";
-};
+export const evenOrOdd = (number) => (number % 2 === 0 ? "Even" : "Odd");
 
 export const multiply = (x, y) => x * y;
