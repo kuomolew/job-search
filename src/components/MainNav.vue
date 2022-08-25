e0727808ebd391edd456e6058c50a2b6079060b9
V.Mironov <kuomolew@gmail.com>
2022-08-25 19:17:55 +0300

    add menu and tailwind config update

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,7 +3,22 @@
     <div
       class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
     >
-      <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+      <a :href="url" target="blank" class="flex items-center h-full text-xl">{{
+        company
+      }}</a>
+      <nav class="h-full ml-12">
+        <ul class="flex h-full p-0 m-0 list-none">
+          <li
+            v-for="(menuItem, i) in menuItems"
+            :key="i"
+            class="h-full ml-9 first:ml-0"
+          >
+            <a href="" class="flex items-center h-full py-2 5">{{
+              menuItem
+            }}</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -14,6 +29,15 @@ export default {
   data() {
     return {
       company: "Careers",
+      url: "https://careers.google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Corp",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
