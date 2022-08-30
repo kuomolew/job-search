c137632b097e586e387ae66ec580e649ef5c8d3c
V.Mironov <kuomolew@gmail.com>
2022-08-30 21:19:40 +0300

    props, @apply

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,27 @@
 <template>
-  <button
-    class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
-  >
-    Sign in
+  <button :class="{ primary }">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: ["text"],
+  data() {
+    return {
+      primary: true,
+    };
+  },
 };
 </script>
+
+<style scoped>
+button {
+  @apply px-5 py-3 font-medium rounded;
+}
+
+.primary {
+  @apply text-white bg-brand-blue-1 hover:shadow-blue;
+}
+</style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -22,7 +22,12 @@
       </nav>
       <div class="flex items-center h-full ml-auto">
         <profile-image v-if="isLoggedIn" data-test="profile-image" />
-        <action-button v-else data-test="login-button" @click="loginUser" />
+        <action-button
+          v-else
+          :text="loginText"
+          data-test="login-button"
+          @click="loginUser"
+        />
       </div>
     </div>
   </div>
@@ -51,6 +56,7 @@ export default {
         "Jobs",
       ],
       isLoggedIn: false,
+      loginText: "Sign in",
     };
   },
   methods: {
