1bdb3e164d0b034a6e409b8ff4ae8bfd70ea4963
V.Mironov <kuomolew@gmail.com>
2022-08-31 19:13:47 +0300

    dynamic object properties

--- src/playground.js
+++ src/playground.js
@@ -1,6 +1,14 @@
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-let squareNumbers = numbers.map((number) => number * number);
+// let squareNumbers = numbers.map((number) => number * number);
 
-console.log("numbers", numbers);
-console.log("squareNumbers", squareNumbers);
+// console.log("numbers", numbers);
+// console.log("squareNumbers", squareNumbers);
+
+const favoriteFood = "sushi";
+
+const goodFood = {
+  [favoriteFood]: true,
+};
+
+console.log(goodFood);
